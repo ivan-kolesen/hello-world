67edfc1266204f715ddaa536388ec2bb70d9019b
vano <kolesen.ivan@gmail.com>
2018-08-25 15:19:40 +0300

    sd

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h2>sdsdsdsd</h2>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To gets started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
